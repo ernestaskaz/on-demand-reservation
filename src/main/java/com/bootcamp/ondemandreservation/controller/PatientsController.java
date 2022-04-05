f87651603ba5df142a7328e0c2693156f71ff42a
AleksandrsC <mirrorfrequency@gmail.com>
2022-04-05 14:53:56 +0300

    basic patients demo

--- src/main/java/com/bootcamp/ondemandreservation/controller/PatientsController.java
+++ src/main/java/com/bootcamp/ondemandreservation/controller/PatientsController.java
@@ -2,12 +2,13 @@ package com.bootcamp.ondemandreservation.controller;
 
 import com.bootcamp.ondemandreservation.model.Patient;
 import com.bootcamp.ondemandreservation.service.PatientService;
+import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.thymeleaf.expression.Maps;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -23,9 +24,8 @@ public class PatientsController {
 
     @GetMapping("/patients")
     String getPatients(Model model){
-        // Map<Long, Patient> =
-        //        patientService.getAllPatients()
-        //model.*/
+        List<Patient>  patients=patientService.getAllPatients();
+        model.addAttribute("patients", patients);
         return "allPatientsView";
     }
 }
