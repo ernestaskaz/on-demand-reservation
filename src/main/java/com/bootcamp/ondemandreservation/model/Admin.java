e07cc9dc8dabbfaf31b1202fa681d61a03f71261
AleksandrsC <mirrorfrequency@gmail.com>
2022-04-05 13:36:55 +0300

    rename of packages and apis in preparation for HTML

--- src/main/java/com/bootcamp/ondemandreservation/model/Admin.java
+++ src/main/java/com/bootcamp/ondemandreservation/model/Admin.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 @Entity
 public class Admin extends ODRUser{
     public Admin() {
+
     }
 
     public Admin(Long id, String email, String password, String firstName, String lastName) {
