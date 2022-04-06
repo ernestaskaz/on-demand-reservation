1344f3532338f108be62dca413268c49e2843bd6
ErnestasK <ernestas1321@gmail.com>
2022-04-06 17:56:11 +0300

    admin endpoints

--- src/main/java/com/bootcamp/ondemandreservation/controller/AdminWebController.java
+++ src/main/java/com/bootcamp/ondemandreservation/controller/AdminWebController.java
@@ -1,10 +1,9 @@
 package com.bootcamp.ondemandreservation.controller;
 
+import com.bootcamp.ondemandreservation.model.Appointment;
 import com.bootcamp.ondemandreservation.model.Doctor;
-import com.bootcamp.ondemandreservation.service.AdminService;
-import com.bootcamp.ondemandreservation.service.DoctorService;
-import com.bootcamp.ondemandreservation.service.ODRUserService;
-import com.bootcamp.ondemandreservation.service.PatientService;
+import com.bootcamp.ondemandreservation.model.Patient;
+import com.bootcamp.ondemandreservation.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -15,11 +14,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/web/")
 public class AdminWebController {
+    public static final String DOCTOR_GET_ALL = "/admin/all-doctors";
+    public static final String APPOINTMENT_GET_ALL = "/admin/all-appointments";
+    public static final String APPOINTMENT_GET_ALL_TODAY = "/admin/today-appointments";
     public static final String DOCTOR_CREATE_URL = "/doctor/create";
     public static final String DOCTOR_CREATE_TEMPLATE = "doctorCreate";
     public static final String ADMIN_ROLE = "hasRole('ROLE_ADMIN')";
@@ -31,12 +34,15 @@ public class AdminWebController {
     private DoctorService doctorService;
     @Autowired
     PatientService patientService;
+    @Autowired
+    private AppointmentService appointmentService;
 
-    public AdminWebController(AdminService adminService, ODRUserService odrUserService, DoctorService doctorService, PatientService patientService) {
+    public AdminWebController(AdminService adminService, ODRUserService odrUserService, DoctorService doctorService, PatientService patientService, AppointmentService appointmentService) {
         this.adminService = adminService;
         this.odrUserService = odrUserService;
         this.doctorService = doctorService;
         this.patientService = patientService;
+        this.appointmentService = appointmentService;
     }
     @GetMapping(DOCTOR_CREATE_URL)
     @PreAuthorize(ADMIN_ROLE)
@@ -63,6 +69,30 @@ public class AdminWebController {
                                                                             doctor.getId()));
         }
         return DOCTOR_CREATE_TEMPLATE;
+    }
+
+    @GetMapping(DOCTOR_GET_ALL)
+    @PreAuthorize(ADMIN_ROLE)
+    String getAllDoctors(Model model) {
+        List<Doctor> doctors = doctorService.getAllDoctors();
+        model.addAttribute("doctors", doctors);
+        return "allDoctorsView";
+    }
+
+    @GetMapping(APPOINTMENT_GET_ALL)
+    @PreAuthorize(ADMIN_ROLE)
+    String getAllAppointments(Model model) {
+        List<Appointment> appointments = appointmentService.getAllAppointments();
+        model.addAttribute("appointments", appointments);
+        return "allAppointmentsView";
+    }
+
+    @GetMapping(APPOINTMENT_GET_ALL_TODAY)
+    @PreAuthorize(ADMIN_ROLE)
+    String getAllTodayAppointments(Model model) {
+        List<Appointment> appointments = appointmentService.getTodaysAppointments();
+        model.addAttribute("appointments", appointments);
+        return "todaysAllAppointments";
     }
 
 }
